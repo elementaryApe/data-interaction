73160cc889a9111f59503ed6f56060ba71ec8835
韩世豪 <hsh@wdtianxia.com>
2018-09-06 16:26:28 +0800

    更新Result

--- di-common/src/main/java/com/herman/di/entity/Result.java
+++ di-common/src/main/java/com/herman/di/entity/Result.java
@@ -1,37 +1,94 @@
 package com.herman.di.entity;
 
+
 /**
  * 返回体封装
+ *
  * @author hsh
  * @create 2018-08-29 16:37
  **/
 public class Result<T> {
+    public final static Integer SUCESS = 200;
+    public final static Integer ERROR = 400;
 
-    private boolean success = false;
-    private String message = null;
-    private T result = null;
 
-    public void isSuccess(boolean b) {
-        this.success = b;
-    }
+    private Integer status;
+    private String message;
+    private T result;
 
-    public void setMessage(String message) {
+
+    public Result(Integer status, String message, T result) {
+        this.status = status;
         this.message = message;
-    }
-
-    public void setResult(T result) {
         this.result = result;
     }
 
-    public boolean getSuccess() {
-        return success;
+    public Result() {
+        this.status = SUCESS;
+        this.message = Message.SUCESS.description;
+    }
+
+    public static Result returnError(String message) {
+        return new Result(ERROR, message, null);
+    }
+
+    public static Result resultSucess() {
+        return new Result();
+    }
+
+    public static Result returnOperateFail() {
+        return new Result(ERROR, Message.ERROR_OPERATE_FAIL.description, null);
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
     }
 
     public String getMessage() {
         return message;
     }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     public T getResult() {
         return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    public static enum Message {
+        SUCESS(0, "操作成功"),
+        ERROR_OPERATE_FAIL(0, "操作失败");
+
+        private Integer code;
+        private String description;
+
+        Message(Integer code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+
+        public Result.Message customDescription(String customError) {
+            if (customError != null) {
+                this.description = customError;
+            }
+            return this;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
     }
 }
