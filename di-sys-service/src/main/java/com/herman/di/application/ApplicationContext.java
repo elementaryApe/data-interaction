d0abacd9f475046dcb25e0d93c68d61dfa3e0106
韩世豪 <hsh@wdtianxia.com>
2018-09-11 16:47:52 +0800

    1集成dubbox 2并切换分支 3.优化部分配置

--- di-sys-service/src/main/java/com/herman/di/application/ApplicationContext.java
+++ di-sys-service/src/main/java/com/herman/di/application/ApplicationContext.java
@@ -1,8 +1,10 @@
 package com.herman.di.application;
 
+import com.herman.di.util.DateUtils;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.text.MessageFormat;
+import java.util.Date;
 
 /**
  * dubbo 启动上下文
@@ -24,6 +26,7 @@ public class ApplicationContext {
     }
 
     public static void main(String[] args) throws Exception {
+        System.out.println("=======启动 dubbo  start " + DateUtils.format(new Date(), DateUtils.DEFAULTDATETIMEPATTERN) + "==========");
         ApplicationContext.getInstance().start();
     }
 
@@ -31,6 +34,7 @@ public class ApplicationContext {
         if (classPathXmlApplicationContext == null) {
             classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath:config/applicationContext.xml");
             classPathXmlApplicationContext.start();
+            System.out.println("=======启动 dubbo  end："+DateUtils.format(new Date(), DateUtils.DEFAULTDATETIMEPATTERN));
         }
         System.out.println(MessageFormat.format("[STARTED]{0}", this.getClass().getSimpleName()));
         while (true) {
